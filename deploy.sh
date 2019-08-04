c2c9672c8b761eb5b829d9f171ef97e571e04b27
edjwhho <edjwh.ho@gmail.com>
2019-08-04 19:28:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-
+set timeout -1
 set server "ubuntu@ec2-52-53-177-239.us-west-1.compute.amazonaws.com"
 spawn bash -c "sudo scp -i /home/edj/.ssh/aws1.pem /var/lib/jenkins/workspace/pipeline1/myproject/target/umsl-0.0.1-SNAPSHOT.jar  $server:"
 
@@ -11,7 +11,9 @@ expect {
 }
 
 interact
-#/usr/bin/sudo ssh -i "/home/edj/.ssh/aws1.pem" $server "killall java"
-#/usr/bin/sudo ssh -i "/home/edj/.ssh/aws1.pem" $server "java -jar umsl-0.0.1-SNAPSHOT.jar &"
+
+catch wait result
+if [lindex $result 3] { send_user "fail\n";  exit 1 }
+send_user "ok\n"
 
 
