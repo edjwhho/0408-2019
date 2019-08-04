b6bc50ae95e723bbffbf5cac2ef37a43a4cc5600
edjwhho <edjwh.ho@gmail.com>
2019-08-04 18:36:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 server="ubuntu@ec2-52-53-177-239.us-west-1.compute.amazonaws.com"
 /usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "killall java"
+/usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "chmod 777 umsl-0.0.1-SNAPSHOT.jar"
 /usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "java -jar umsl-0.0.1-SNAPSHOT.jar &"
