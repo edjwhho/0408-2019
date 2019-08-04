b93f3737f25455710350bf3f7245aec4f9bd0f3d
edjwhho <edjwh.ho@gmail.com>
2019-08-04 20:24:39 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-set timeout -1
+set timeout 600
 set server "ubuntu@ec2-52-53-177-239.us-west-1.compute.amazonaws.com"
 spawn bash -c "sudo scp -i /home/edj/.ssh/aws1.pem /var/lib/jenkins/workspace/pipeline1/myproject/target/umsl-0.0.1-SNAPSHOT.jar  $server:"
 
@@ -9,8 +9,6 @@ expect {
     exp_continue
   }
 }
-
-interact
 
 
 
