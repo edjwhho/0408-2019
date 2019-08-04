7b260d3ceb5515f80cbfe5d391d458e1d3bace50
edjwhho <edjwh.ho@gmail.com>
2019-08-04 21:30:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 server="ubuntu@ec2-52-53-177-239.us-west-1.compute.amazonaws.com"
-timeout 5 /usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "killall java"
-timeout 5 /usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "chmod 777 umsl-0.0.1-SNAPSHOT.jar"
-timeout 10 /usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "java -jar umsl-0.0.1-SNAPSHOT.jar &"
+/usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "killall java"
+/usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "chmod 777 umsl-0.0.1-SNAPSHOT.jar"
+/usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "/usr/bin/nohup java -jar umsl-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &"
+exit 0
