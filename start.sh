c374952ed5e3a659db9d8577687b78085221a6f7
edjwhho <edjwh.ho@gmail.com>
2019-08-04 17:29:43 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+server="ubuntu@ec2-52-53-177-239.us-west-1.compute.amazonaws.com"
+
+/usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "killall java"
+/usr/bin/sudo /usr/bin/ssh -i "/home/edj/.ssh/aws1.pem" $server "java -jar umsl-0.0.1-SNAPSHOT.jar &"
